baeedd7316640f4dcfc3dcfd6a1c9a2846f39ad8
Cheanggi <chea.19066@mhs.unesa.ac.id>
2022-10-24 23:25:07 +0700

    new fileku

--- script2.js
+++ script2.js
@@ -1,6 +1,8 @@
 const listCardTemplate = document.querySelector("[list-dokter-template]")
 const listCardContainer = document.querySelector("[list-dokter-container]")
 const searchInput = document.querySelector("[data-search]")
+const keyID = document.getElementById("card")
+let it = 0;
 
 let lists = []
 
@@ -26,7 +28,19 @@ fetch("https://6354e3f4da523ceadcf5c060.mockapi.io/lists")
             text.textContent = list.spesialis
             cardText.textContent = list.tahun
             listCardContainer.append(card)
-            return { name: list.name, spesialis: list.spesialis, element: card }
+            return { name: list.name, spesialis: list.spesialis, element: card, image: list.image}
+            
+            
         })
+        console.log(lists.length)
+        console.log(keyID)
+        
+        
+        lists.index = it;
+        console.log(lists)
+        console.log(typeof lists)
+        let listString = JSON.stringify(lists);
+        localStorage.setItem('keyDokter', listString);
+        
     })
 
